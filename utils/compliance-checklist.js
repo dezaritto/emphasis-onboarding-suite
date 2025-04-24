6c871eb6fd6cb0c0f60122bc72f6fa7b6a6e0cc5
dezaritto <dez@emphasiswealth.co.za>
2025-04-24 10:30:13 +0200

    Create compliance-checklist.js

---
+++ utils/compliance-checklist.js
@@ -0,0 +1,10 @@
+// utils/compliance-checklist.js
+
+const COMPLIANCE_CHECKLIST = [
+  "Signed FAIS Disclosure",
+  "Signed Appropriate Service Agreement",
+  "Verified FICA Documents",
+  "Record of Advice (RoA) Issued"
+];
+
+export default COMPLIANCE_CHECKLIST;
